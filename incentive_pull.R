4065947f91a31c47acf941dbf07db5fee76e08d1
Isaiah Westphalen <iwestphalen@guidehouse.com>
2022-12-20 07:12:25 -0500

    dont need this code anymore

--- incentive_pull.R
+++ incentive_pull.R
@@ -1,30 +1,30 @@
-library(tidyverse)
-library(reticulate)
-source("secret_key.R")
-
-pd <- import("pandas")
-requests <- import("requests")
-
-source_python("get_incentives.py")
-
-incent <- get_incentives()
-
-inc_type <- c(
-  "Incentives",
-  "State Incentives",
-  "Utility/Private Incentives"
-)
-
-e_cent <- incent %>%
-  mutate(technologies = ifelse(lengths(technologies) == 0,
-                               "",
-                               technologies)) %>%
-  unnest(c("technologies")) %>%
-  unnest(c("county_ids", "title")) %>%
-  filter(technologies == "ELEC",
-         type %in% inc_type) %>%
-  left_join(incent %>% pull(references) %>% bind_rows()) %>%
-  select(-topics,
-         -text,
-         -enacted_date)
-
+# library(tidyverse)
+# library(reticulate)
+# source("secret_key.R")
+#
+# pd <- import("pandas")
+# requests <- import("requests")
+#
+# source_python("get_incentives.py")
+#
+# incent <- get_incentives()
+#
+# inc_type <- c(
+#   "Incentives",
+#   "State Incentives",
+#   "Utility/Private Incentives"
+# )
+#
+# e_cent <- incent %>%
+#   mutate(technologies = ifelse(lengths(technologies) == 0,
+#                                "",
+#                                technologies)) %>%
+#   unnest(c("technologies")) %>%
+#   unnest(c("county_ids", "title")) %>%
+#   filter(technologies == "ELEC",
+#          type %in% inc_type) %>%
+#   left_join(incent %>% pull(references) %>% bind_rows()) %>%
+#   select(-topics,
+#          -text,
+#          -enacted_date)
+#
