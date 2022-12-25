17b13bdbf3088f36b54c2ce9f4581ea802ae7169
isaiah westphalen <isaiahwestphalen@gmail.com>
2022-12-24 21:49:11 -0500

    adding changing datetime column

--- pull_dc_chargers.py
+++ pull_dc_chargers.py
@@ -1,8 +1,15 @@
 #!/usr/bin/env python
 
 import pandas as pd
+from datetime import datetime
 from nearest_ev_stations import nearest_ev_stations
 
+now = datetime.now()
+
+current_time = now.strftime("%H:%M:%S")
+
 dc_charge = nearest_ev_stations()[0]
+
+dc_charge["time_update"] = current_time
 
 dc_charge.to_csv("dc_charge.csv")
